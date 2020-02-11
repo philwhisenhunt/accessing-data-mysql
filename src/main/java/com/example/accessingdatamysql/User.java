fb753752690d8c1e0f1dc0872d89e39e666bdde5
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-11 11:09:06 -0500

    Wrapping up entity model

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -23,4 +23,19 @@ public class User {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
